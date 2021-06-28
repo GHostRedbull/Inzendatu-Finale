c8d29fe81181ff16b8948f9d08a272630bf5e21e
Jalal <jalal.hilal@edu.itescia.fr>
2021-06-28 14:20:37 +0200

    Fix Arrondi + quelques modifs

--- Inzendatu_Version1/Form1.cs
+++ Inzendatu_Version1/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,6 +14,19 @@ namespace Inzendatu_Version1
 {
     public partial class Form1 : Form
     {
+        /// Arrondie les coins de la Form manuellement
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // width of ellipse
+            int nHeightEllipse // height of ellipse
+        );
+
+
         private List<string> listFilesSelected = new List<string>();
         private bool checkPrev = false;
         private string Pathnow = "";
@@ -20,13 +34,16 @@ namespace Inzendatu_Version1
         private Bunifu.UI.WinForms.BunifuDataGridView gridPrev = new Bunifu.UI.WinForms.BunifuDataGridView();
         private Boolean ouvertoupas = false;
         private bool radioisChecked = false;
+        private bool check_State = false;
 
         public Form1()
         {
             InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 9, 9)); //Creer manuellement les corner arrondis
+        }
 
-            listView1.MultiSelect = true;
-
+        private void Form1_Load(object sender, EventArgs e)
+        {
             bunifuDataGridView1.Columns.Add("Id", "Numero");
             bunifuDataGridView1.Columns.Add("Nom", "Nom du ficher");
             bunifuDataGridView1.Columns[0].Width = 100;
@@ -35,39 +52,21 @@ namespace Inzendatu_Version1
             gridPrev.Columns.Add("NO", "Nom Original");
             gridPrev.Columns.Add("NN", "Nouveau Nom");
 
-            bunifuLabel1.Text = "";// Nom (Titre)
-            bunifuLabel2.Text = ""; // Emplacement (Titre)
-            bunifuLabel3.Text = ""; // Type (Titre)
-            bunifuLabel4.Text = ""; // Taille (Titre)
-            bunifuLabel5.Text = ""; // Dimensions (Titre)
-            bunifuLabel6.Text = ""; // D.Creation (Titre)
-            bunifuLabel7.Text = ""; // D.Modifi (Titre)
-            bunifuLabel8.Text = "";// Nom
-            bunifuLabel9.Text = ""; // Emplacement
-            bunifuLabel10.Text = ""; // Type
-            bunifuLabel11.Text = ""; // Taille
-            bunifuLabel12.Text = ""; // Dimensions
-            bunifuLabel13.Text = ""; // D.Creation
-            bunifuLabel14.Text = ""; // D.Modifi
+            initLabelVide();
 
-            listView1.View = View.List;
-
+            listView1.MultiSelect = true;
+            listView1.View = View.List;    
             bunifuVScrollBar1.BindTo(bunifuDataGridView1);
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-           
         }
 
         private void bunifuTileButton3_Click(object sender, EventArgs e)
         {
-            listFilesSelected.Clear();
-            listView1.Items.Clear();
             using (FolderBrowserDialog folderDialog = new FolderBrowserDialog() { Description = "Select tour path" })
             {
                 if (folderDialog.ShowDialog() == DialogResult.OK) { 
                     Pathnow = folderDialog.SelectedPath;
+                    listFilesSelected.Clear();
+                    listView1.Items.Clear();
                     foreach (string item in Directory.GetFiles(folderDialog.SelectedPath))
                     {
                         imageList1.Images.Add(Icon.ExtractAssociatedIcon(item));
@@ -88,40 +87,25 @@ namespace Inzendatu_Version1
 
         private void bunifuDataGridView1_SelectionChanged(object sender, EventArgs e)
         {
-            if (bunifuDataGridView1.SelectedRows.Count == 1 && ouvertoupas == true && bunifuDataGridView1.RowCount > 1)
+            /// Clean la description 
+            bool check = false;
+            try { if (bunifuDataGridView1.CurrentRow.Cells[0].Value.ToString() != string.Empty) { } }
+            catch (Exception ee)
+            {
+                if (ee.ToString().Contains("System.NullReferenceException: La référence d'objet n'est pas définie à une instance d'un objet."))
+                {
+                    check = true;
+                    initLabelVide();
+                }
+            }
+
+            if (bunifuDataGridView1.SelectedRows.Count == 1 && ouvertoupas == true && bunifuDataGridView1.RowCount > 1 && check == false)
             {
                 try
                 {
                     string activePath = getEmplacement(bunifuDataGridView1.SelectedRows[0].Index);
                     FileInfo fileInfo = new FileInfo(activePath);
-
-                    bunifuLabel1.Text = "Nom";// Nom (Titre)
-                    bunifuLabel2.Text = "Emplacement"; // Emplacement (Titre)
-                    bunifuLabel3.Text = "Type"; // Type (Titre)
-                    bunifuLabel4.Text = "Taille"; // Taille (Titre)
-                    bunifuLabel5.Text = "Dimensions"; // Dimensions (Titre)
-                    bunifuLabel6.Text = "Creation"; // D.Creation (Titre)
-                    bunifuLabel7.Text = "Modifications"; // D.Modifi (Titre)
-                    bunifuLabel8.Text = fileInfo.Name; // Nom
-                    bunifuLabel9.Text = fileInfo.Directory.ToString(); // Emplacement
-                    bunifuLabel10.Text = fileInfo.Extension; // Type
-                    bunifuLabel11.Text = fileInfo.Length.ToString(); // Taille du fichier
-                    if (fileInfo.Length >= (1 << 30))
-                        bunifuLabel11.Text = string.Format("{0} Go", fileInfo.Length >> 30);
-                    else
-                    if (fileInfo.Length >= (1 << 20))
-                        bunifuLabel11.Text = string.Format("{0} Mo", fileInfo.Length >> 20);
-                    else
-                    if (fileInfo.Length >= (1 << 10))
-                        bunifuLabel11.Text = string.Format("{0} Ko", fileInfo.Length >> 10);
-                    else
-                        bunifuLabel11.Text = string.Format("{0} Octets", fileInfo.Length >> 10);
-
-                    bunifuLabel12.Text = getDimensions(activePath); // Dimensions
-                    bunifuLabel13.Text = fileInfo.CreationTime.ToString(); // D.Creation
-                    bunifuLabel14.Text = fileInfo.LastWriteTime.ToString(); // D.Modifi
-
-                    pictureBox3.Image = Icon.ExtractAssociatedIcon(activePath).ToBitmap();
+                    initLabelRempli(fileInfo, activePath);
                 }
                 catch (Exception ee)
                 {
@@ -176,13 +160,25 @@ namespace Inzendatu_Version1
 
         private void bunifuButton2_Click(object sender, EventArgs e)
         {
-            Bunifu.UI.WinForms.BunifuTransition transition = new Bunifu.UI.WinForms.BunifuTransition();
-
+            //// Fix pour le focus du bunifuBouton2 "Previsualiser" -> Pressed/unpressed
+            if (check_State == false)
+            {
+                bunifuButton2.FocusState = Bunifu.UI.WinForms.BunifuButton.BunifuButton.ButtonStates.Idle;
+                bunifuButton2.FocusState = Bunifu.UI.WinForms.BunifuButton.BunifuButton.ButtonStates.Pressed;
+                check_State = true;
+            }
+            else
+            {
+                bunifuButton2.FocusState = Bunifu.UI.WinForms.BunifuButton.BunifuButton.ButtonStates.Idle;
+                check_State = false;
+            }
+            ////
+            
             if (checkPrev == false)
             {
                 panelPrev.Size = new Size(1400, 250);
                 panelPrev.Dock = DockStyle.Bottom;
-                panelPrev.BackgroundColor = Color.Red;
+                panelPrev.Padding = new Padding(12, 0, 12, 12);
                 panelPrev.Controls.Add(gridPrev);
 
                 Bunifu.Framework.UI.BunifuElipse elipse = new Bunifu.Framework.UI.BunifuElipse();
@@ -195,6 +191,9 @@ namespace Inzendatu_Version1
                 gridPrev.AllowUserToOrderColumns = false;
                 gridPrev.ReadOnly = true;
                 gridPrev.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                gridPrev.ColumnHeadersHeight = 35;
+                gridPrev.RowHeadersWidth = 35;
+                gridPrev.RowTemplate.Height = 35;
                 gridPrev.ScrollBars = ScrollBars.None;
                 gridPrev.CurrentTheme.BackColor = Color.White;
                 gridPrev.CurrentTheme.GridColor = Color.White;
@@ -213,17 +212,16 @@ namespace Inzendatu_Version1
                 gridPrev.HeaderBackColor = Color.FromArgb(57, 47, 90);
                 gridPrev.HeaderForeColor = Color.White;
 
-                this.Controls.Add(panelPrev);
                 this.Size = new Size(1400, 1050);
-                //transition.ShowSync(this, false, Bunifu.UI.WinForms.BunifuAnimatorNS.Animation.Transparent);
-
+                Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 9, 9)); //Creer manuellement les corner arrondis
+                this.Controls.Add(panelPrev);
                 checkPrev = true;
             }
             else
             {
                 this.Size = new Size(1400, 800);
                 this.Controls.Remove(panelPrev);
-
+                Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 9, 9)); //Creer manuellement les corner arrondis
                 checkPrev = false;
             }
         }
@@ -248,23 +246,58 @@ namespace Inzendatu_Version1
         {
             if (bunifuDataGridView1.Rows.Count == 1)
             {
-                bunifuLabel1.Text = "";// Nom (Titre)
-                bunifuLabel2.Text = ""; // Emplacement (Titre)
-                bunifuLabel3.Text = ""; // Type (Titre)
-                bunifuLabel4.Text = ""; // Taille (Titre)
-                bunifuLabel5.Text = ""; // Dimensions (Titre)
-                bunifuLabel6.Text = ""; // D.Creation (Titre)
-                bunifuLabel7.Text = ""; // D.Modifi (Titre)
-                bunifuLabel8.Text = "";// Nom
-                bunifuLabel9.Text = ""; // Emplacement
-                bunifuLabel10.Text = ""; // Type
-                bunifuLabel11.Text = ""; // Taille
-                bunifuLabel12.Text = ""; // Dimensions
-                bunifuLabel13.Text = ""; // D.Creation
-                bunifuLabel14.Text = ""; // D.Modifi
-                pictureBox3.Image = null;
+                initLabelVide();
             }
             bunifuVScrollBar1.Maximum = bunifuDataGridView1.RowCount + 5;
+        }
+
+        private void initLabelVide()
+        {
+            bunifuLabel1.Text = "";// Nom (Titre)
+            bunifuLabel2.Text = ""; // Emplacement (Titre)
+            bunifuLabel3.Text = ""; // Type (Titre)
+            bunifuLabel4.Text = ""; // Taille (Titre)
+            bunifuLabel5.Text = ""; // Dimensions (Titre)
+            bunifuLabel6.Text = ""; // D.Creation (Titre)
+            bunifuLabel7.Text = ""; // D.Modifi (Titre)
+            bunifuLabel8.Text = "";// Nom
+            bunifuLabel9.Text = ""; // Emplacement
+            bunifuLabel10.Text = ""; // Type
+            bunifuLabel11.Text = ""; // Taille
+            bunifuLabel12.Text = ""; // Dimensions
+            bunifuLabel13.Text = ""; // D.Creation
+            bunifuLabel14.Text = ""; // D.Modifi
+            pictureBox1.Image = null;
+            pictureBox3.Image = null;
+        }
+
+        private void initLabelRempli(FileInfo fi, string path_)
+        {
+            bunifuLabel1.Text = "Nom";// Nom (Titre)
+            bunifuLabel2.Text = "Emplacement"; // Emplacement (Titre)
+            bunifuLabel3.Text = "Type"; // Type (Titre)
+            bunifuLabel4.Text = "Taille"; // Taille (Titre)
+            bunifuLabel5.Text = "Dimensions"; // Dimensions (Titre)
+            bunifuLabel6.Text = "Creation"; // D.Creation (Titre)
+            bunifuLabel7.Text = "Modifications"; // D.Modifi (Titre)
+            bunifuLabel8.Text = fi.Name; // Nom
+            bunifuLabel9.Text = fi.Directory.ToString(); // Emplacement
+            bunifuLabel10.Text = fi.Extension; // Type
+            bunifuLabel11.Text = fi.Length.ToString(); // Taille
+            if (fi.Length >= (1 << 30))
+                bunifuLabel11.Text = string.Format("{0} Go", fi.Length >> 30);
+            else
+                    if (fi.Length >= (1 << 20))
+                bunifuLabel11.Text = string.Format("{0} Mo", fi.Length >> 20);
+            else
+                    if (fi.Length >= (1 << 10))
+                bunifuLabel11.Text = string.Format("{0} Ko", fi.Length >> 10);
+            else
+                bunifuLabel11.Text = string.Format("{0} Octets", fi.Length >> 10);
+            bunifuLabel12.Text = getDimensions(path_); // Dimensions
+            bunifuLabel13.Text = fi.CreationTime.ToString(); // D.Creation
+            bunifuLabel14.Text = fi.LastWriteTime.ToString(); // D.Modifi
+            pictureBox3.Image = Icon.ExtractAssociatedIcon(path_).ToBitmap();
         }
     }
 }
